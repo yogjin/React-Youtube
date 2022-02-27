1947fcd8af8fd9fe338c44d14539c8aee10bb09e
yogjin <youngjin988@gmail.com>
2022-02-28 02:53:33 +0900

    fix: Video Component key를 video의 id로 설정
    처음 렌더링할때 사용하는 video들:(Most popular:GET)은 video id를 감싸지않고 가지고 있고, 검색결과 받아오는 video들:(Search:GET)은 video id가 객체로 감싸져 있다 따라서 두가지 상황에 맞게 key를 설정. 또한 검색결과로 채널이 나오는 경우가 있는데 이때 추후 처리가 필요함

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -6,7 +6,10 @@ const Videos = ({ videos }) => {
   return (
     <main className={styles.videos}>
       {videos.map((video) => (
-        <Video key={video.id} snippet={video.snippet} />
+        <Video
+          key={video.id.videoId ? video.id.videoId : video.id}
+          snippet={video.snippet}
+        />
       ))}
     </main>
   );
