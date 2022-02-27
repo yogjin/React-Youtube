8a153273ef2b5604e026f8795d033725050f98be
yogjin <youngjin988@gmail.com>
2022-02-28 00:46:29 +0900

    refactor: Move list(state) to App.jsx from Videos.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Videos from './components/Videos';
 
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+};
+
 function App() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const fetchVideos = () =>
+      fetch(
+        'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBs8Sv9Ix-HQ6pgs1-yhfhBby3TPbjUTls',
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((videos) => setList(videos.items))
+        .catch((error) => console.log('error', error));
+    fetchVideos();
+  }, []);
+
   return (
     <>
       <Header />
-      <Videos />
+      <Videos list={list} />
     </>
   );
 }
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,27 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Video from './Video';
 import styles from '../styles/Videos.module.css';
 
-const requestOptions = {
-  method: 'GET',
-  redirect: 'follow',
-};
-
-const Videos = (props) => {
-  const [list, setList] = useState([]);
-
-  useEffect(() => {
-    const fetchVideos = () =>
-      fetch(
-        'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBs8Sv9Ix-HQ6pgs1-yhfhBby3TPbjUTls',
-        requestOptions
-      )
-        .then((response) => response.json())
-        .then((videos) => setList(videos.items))
-        .catch((error) => console.log('error', error));
-    fetchVideos();
-  }, []);
-
+const Videos = ({ list }) => {
   return (
     <main className={styles.videos}>
       {list.map((video) => (
