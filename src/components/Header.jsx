31256176f6b4f0c70262e69a42b755d59d9e2f99
yogjin <youngjin988@gmail.com>
2022-02-27 16:01:14 +0900

    prettier

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,18 @@ import styles from '../styles/Header.module.css';
 const Header = (props) => {
   return (
     <header className={styles.header}>
-      <img src="/images/logo.png" alt="youtube-logo" className="header__logo" />
+      <img
+        src="/images/logo.png"
+        alt="youtube-logo"
+        className={styles.header__logo}
+      />
       <span>Youtube</span>
-      <form className="header__search" action="">
-        <input type="text" placeholder="검색" />
+      <form className={styles.header__search} action="">
+        <input
+          className={styles.header__input}
+          type="text"
+          placeholder="검색"
+        />
         <button>검색</button>
       </form>
     </header>
