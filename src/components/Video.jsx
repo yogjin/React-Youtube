3dc67f708596e55f9dc11efa270433f402c75390
yogjin <youngjin988@gmail.com>
2022-02-28 00:27:48 +0900

    style: Design Videos for dev

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -5,9 +5,15 @@ const Video = ({ snippet }) => {
   const { publishedAt, title, description, thumbnails, channelTitle } = snippet;
   return (
     <article className={styles.video}>
-      <img src={thumbnails.high.url} alt="thumbnails" />
-      <span className={styles.video__title}>{title}</span>
-      <span className={styles.video__channelTitle}>{channelTitle}</span>
+      <img
+        className={styles.video__thumbnails}
+        src={thumbnails.high.url}
+        alt="thumbnails"
+      />
+      <div className={styles.video__metadata}>
+        <span className={styles.video__title}>{title}</span>
+        <span className={styles.video__channelTitle}>{channelTitle}</span>
+      </div>
     </article>
   );
 };
