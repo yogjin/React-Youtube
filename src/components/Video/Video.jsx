a85586109f5a62bf3aa779aeaccfde12b3a3b853
yogjin <youngjin988@gmail.com>
2022-03-03 01:11:27 +0900

    style(Video): 비디오 실행여부에 따라 Video Component의 style변경

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -13,11 +13,11 @@ const Video = ({ videoId, snippet, handleVideoClick, display }) => {
       }
     >
       <img
-        className={styles.video__thumbnails}
+        className={`${styles.video__thumbnails} ${displayType}`}
         src={thumbnails.medium.url}
         alt="thumbnails"
       />
-      <div className={styles.video__metadata}>
+      <div className={`${styles.video__metadata} ${displayType}`}>
         <span className={styles.video__title}>{title}</span>
         <span className={styles.video__channelTitle}>{channelTitle}</span>
       </div>
