529659bb7fe5124d6ef22cabaae3b9fdee84375e
yogjin <youngjin988@gmail.com>
2022-03-03 19:27:26 +0900

    refactor: Use axios instead of fetch

--- src/services/youtube.js
+++ src/services/youtube.js
@@ -1,32 +1,34 @@
 // youtube API fetch module
+import axios from 'axios';
 
 export default class Youtube {
   constructor(key) {
-    this.key = key;
-    this.requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
+    this.youtube = axios.create({
+      baseURL: 'https://youtube.googleapis.com/youtube/v3',
+      params: { key },
+    });
   }
   // 첫 화면 인기동영상
   async mostPopular() {
-    return fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=28&key=${this.key}`,
-      this.requestOptions
-    )
-      .then((response) => response.json())
-      .then((videos) => videos.items)
-      .catch((error) => console.log('error', error));
+    const response = await this.youtube
+      .get('videos', {
+        params: { part: 'snippet', chart: 'mostPopular', maxResults: 28 },
+      })
+      .catch((error) => console.log(`${error} | ${new Date()}`));
+    return response.data.items;
   }
 
   // 검색
-  search(query) {
-    return fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&key=${this.key}`,
-      this.requestOptions
-    )
-      .then((response) => response.json())
-      .then((videos) => videos.items)
-      .catch((error) => console.log('error', error));
+  async search(query) {
+    const response = await this.youtube
+      .get('search', {
+        params: {
+          part: 'snippet',
+          maxResults: 28,
+          q: query,
+        },
+      })
+      .catch((error) => console.log(`${error} | ${new Date()}`));
+    return response.data.items;
   }
 }
